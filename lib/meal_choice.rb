bd23ea9e89c70651aeab80ba61a03e3480bd31ff
Farzad Daud <daud.farzad1@gmail.com>
2020-01-25 16:15:58 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,12 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
+Learn more or give us feedback
+describe '#meal_choice' do
+  it 'should default to meat' do
+    expect(meal_choice).to eq("steak")
+  end
+
+  it 'should allow you to set a meal' do
+    expect(meal_choice("vegan")).to eq("vegan")
+  end
+end
